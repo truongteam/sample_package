3d558acfb32f08d9b1c0344224d7e375d34e46fc
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2021-08-14 22:30:26 +0700

    add typings

--- index.js
+++
@@ -1,5 +0,0 @@
-function sayHello(msg) {
-    return `Hello ${msg}`
-}
-
-module.exports = sayHello;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string}  msg - A string param.
+ * @return {void} This is the result
+ */
+function sayHello(msg) {
+    return `Hello ${msg}`
+}
+
+module.exports = sayHello;
